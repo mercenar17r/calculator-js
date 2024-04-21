3015f9e0ff4ae6a11112972430a99e04bc7996d6
Negrut Cosmin <cosminnegrut776@gmail.com>
2024-04-21 23:48:17 +0300

    practice with functions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,15 @@
-let currentResult = 0;
+const defaultResult=0;
+function add (num1,num2){
+    const result=num1+num2;
+    alert('The result is'  + result);
 
-currentResult= currentResult + 0;
- outputResult (currentResult, '');
+}
+add(6,7);
+
+
+let currentResult = defaultResult;
+currentResult=(currentResult + 10)* 3 /2 -1;
+
+
+let calculationDescription =`( ${defaultResult} + 10) * 3 /2 -1` ;
+ outputResult (currentResult, calculationDescription);
