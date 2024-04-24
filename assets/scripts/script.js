5b9d5d5663f9554eb9368f5f9757bdf7126d4ae2
Negrut Cosmin <cosminnegrut776@gmail.com>
2024-04-24 23:48:40 +0300

    clear code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,14 @@
 const defaultResult=0;
+let currentResult=defaultResult;
 function add (num1,num2){
     const result=num1+num2;
     alert('The result is'  + result);
-return add
+return result
 }
-let result=add(2,5)
+
+currentResult=add(1,2)
 
 
-
-let currentResult = defaultResult;
 currentResult=(currentResult + 10)* 3 /2 -1;
 
 
